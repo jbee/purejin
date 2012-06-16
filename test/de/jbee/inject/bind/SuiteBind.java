4fd0d63822c69624147eb8713797252305066dcc
Jan Bernitt <jan.bernitt@gmx.de>
2012-06-16 20:10:55 +0200

    DOC service decoupling example

--- test/de/jbee/inject/bind/SuiteBind.java
+++ test/de/jbee/inject/bind/SuiteBind.java
@@ -6,8 +6,8 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith ( Suite.class )
 @SuiteClasses ( { TestAutobindBinds.class, TestElementBinds.class, TestInstanceBinds.class,
-		TestRobotLegsBinds.class, TestServiceBinds.class, TestSupplierBinds.class,
-		TestTypeBinds.class } )
+		TestRobotLegsBinds.class, TestServiceBinds.class, TestServiceDecouplerBinds.class,
+		TestSupplierBinds.class, TestTypeBinds.class } )
 public class SuiteBind {
 
 }
--- test/de/jbee/inject/bind/TestServiceDecouplerBinds.java
+++ test/de/jbee/inject/bind/TestServiceDecouplerBinds.java
@@ -13,29 +13,22 @@ import de.jbee.inject.service.ServiceModule;
 
 public class TestServiceDecouplerBinds {
 
-	static class ServiceDecouplerBindsModule
-			extends ServiceModule {
-
-		@Override
-		protected void configure() {
-			bindServices( SomeService.class );
-			bind( AppService.class, new AppServiceDecoupler() );
-		}
-
-	}
-
-	private static class SomeService {
-
-		Integer square( Integer value ) {
-			return value * value;
-		}
-	}
-
+	/**
+	 * Think of this as an application specific service interface that normally would have been
+	 * defined within your application code. This is what 'your' code asks for whereby you don't get
+	 * any dependencies pointing in direction of the DI framework inside your normal app-code. The
+	 * only place you are coupled to the DI-framework is still in the binding code that is
+	 * additional to the app-code.
+	 */
 	public static interface AppService<P, R> {
 
 		R doIt( P param );
 	}
 
+	/**
+	 * This is an adapter to 'your' application specific service interface adapting to the
+	 * {@link Service} interface of the DI-framework. So internally both are resolvable.
+	 */
 	static class AppServiceDecoupler
 			implements Service.ServiceDecoupler<AppService<?, ?>> {
 
@@ -63,11 +56,40 @@ public class TestServiceDecouplerBinds {
 		}
 	}
 
+	static class ServiceDecouplerBindsModule
+			extends ServiceModule {
+
+		@Override
+		protected void configure() {
+			bindServices( SomeService.class );
+			bind( AppService.class, new AppServiceDecoupler() );
+		}
+
+	}
+
+	private static class SomeService {
+
+		Integer square( Integer value ) {
+			return value * value;
+		}
+	}
+
 	@Test
-	public void test() {
+	public void thatAppServiceCanBeResolvedWhenHavingGenericsInSameOrder() {
 		DependencyResolver injector = Bootstrap.injector( ServiceDecouplerBindsModule.class );
 		AppService<Integer, Integer> service = injector.resolve( Dependency.dependency( Type.raw(
 				AppService.class ).parametized( Integer.class, Integer.class ) ) );
 		assertThat( service.doIt( 2 ), is( 4 ) );
 	}
+
+	@Test
+	public void thatAppServiceCanBeResolvedWhenHavingGenericsInDifferentOrder() {
+		//TODO
+	}
+
+	@Test
+	public void thatAppServiceCanBeResolvedWhenHavingJustOneGeneric() {
+		//TODO
+	}
+
 }
