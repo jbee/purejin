7c6f66e14f695b3c77be28e334860673b1428379
Jan Bernitt <jan.bernitt@gmx.de>
2013-01-27 18:34:56 +0100

    NEW more test for Injectron injections

--- src/test/se/jbee/inject/bind/TestInjectronBinds.java
+++ src/test/se/jbee/inject/bind/TestInjectronBinds.java
@@ -3,6 +3,7 @@ package se.jbee.inject.bind;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static se.jbee.inject.Dependency.dependency;
 import static se.jbee.inject.Name.named;
@@ -16,6 +17,7 @@ import org.junit.Test;
 import se.jbee.inject.Dependency;
 import se.jbee.inject.Injector;
 import se.jbee.inject.Injectron;
+import se.jbee.inject.Name;
 
 public class TestInjectronBinds {
 
@@ -25,8 +27,10 @@ public class TestInjectronBinds {
 		@Override
 		protected void declare() {
 			bind( String.class ).to( "foobar" );
-			bind( named( "special" ), String.class ).to( "special" );
+			Name special = named( "special" );
+			bind( special, String.class ).to( "special" );
 			inPackageOf( List.class ).bind( String.class ).to( "list" );
+			inPackageOf( List.class ).bind( special, String.class ).to( "special-list" );
 		}
 
 	}
@@ -45,7 +49,12 @@ public class TestInjectronBinds {
 	public void thatInjectronArrayIsAvailableForEveryBoundResource() {
 		Dependency<? extends Injectron<String>[]> dependency = dependency( injectronsTypeOf( String.class ) );
 		Injectron<String>[] injectrons = injector.resolve( dependency );
-		assertThat( injectrons.length, is( 3 ) );
+		assertEquals( 4, injectrons.length );
+	}
+
+	@Test
+	public void thatInjectronArrayIsAvailableForAllResources() {
+		assertEquals( 4, injector.resolve( dependency( Injectron[].class ) ).length );
 	}
 
 	@Test
@@ -53,8 +62,9 @@ public class TestInjectronBinds {
 		Dependency<? extends Injectron<String>[]> dependency = dependency(
 				injectronsTypeOf( String.class ) ).named( "special" );
 		Injectron<String>[] injectrons = injector.resolve( dependency );
-		assertThat( injectrons.length, is( 1 ) );
-		assertThat( injectrons[0].instanceFor( dependency( String.class ) ), is( "special" ) );
+		assertEquals( 2, injectrons.length );
+		assertThat( injectrons[0].instanceFor( dependency( String.class ) ), is( "special-list" ) );
+		assertThat( injectrons[1].instanceFor( dependency( String.class ) ), is( "special" ) );
 	}
 
 	@Test
@@ -62,6 +72,4 @@ public class TestInjectronBinds {
 		Injector resolved = injector.resolve( dependency( Injector.class ) );
 		assertThat( resolved, sameInstance( injector ) );
 	}
-
-	//TODO check that array of Injectrons is available 
 }
