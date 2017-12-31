4bb52e2ea4a236edfc25a75fe2fccf033047377f
Jan Bernitt <jaanbernitt@gmail.com>
2017-12-31 12:18:20 +0100

    javadoc

--- src/core/se/jbee/inject/Initialiser.java
+++ src/core/se/jbee/inject/Initialiser.java
@@ -1,17 +1,25 @@
 package se.jbee.inject;
 
 /**
- * User can bind a implementation for this interface. The {@link Injector} will
- * resolve all of them and call their {@link #init(Injector)} method as soon as
- * the context is done.
+ * Users can bind an implementation for this interface. The {@link Injector}
+ * will resolve all of them and call their {@link #init(Injector)} method as
+ * soon as the context is done.
  * 
  * This gives users the possibility to run initialization code once and build
- * more powerful mechanisms on top of it. 
+ * more powerful mechanisms on top of it.
  * 
  * @author jan
  */
 @FunctionalInterface
 public interface Initialiser {
 
+	/**
+	 * Is called by the {@link Injector} as soon as the context itself is
+	 * initialized and ready to be used by the implementation of this interface.
+	 * 
+	 * @param context
+	 *            use to receive instances that require further initialization
+	 *            setup
+	 */
 	void init(Injector context);
 }
