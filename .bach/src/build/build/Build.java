f68afcb5b3a643c3fff6be6ca032661287ed6cb3
Christian Stein <sormuras@gmail.com>
2020-06-06 21:09:26 +0200

    Generate "consumer-pom" for Maven deployments
    Find details at https://cwiki.apache.org/confluence/display/MAVEN/Build+vs+Consumer+POM
    
    (cherry picked from commit d892c9b7207bc9f9155f9abaa9baf8ac20963dbd)

--- .bach/src/build/build/Build.java
+++ .bach/src/build/build/Build.java
@@ -1,16 +1,21 @@
 package build;
 
 import de.sormuras.bach.Bach;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 class Build {
-  public static void main(String... args) {
+  public static void main(String... args) throws Exception {
+    var version = "19.1-ea";
+
     var bach =
         Bach.of(
             scanner -> scanner.offset("src"),
             project ->
                 project
                     .title("Silk DI")
-                    .version("19.1-ea")
+                    .version(version)
                     .requires("org.hamcrest") // By junit at runtime.
                     .requires("org.junit.vintage.engine") // Discovers and executes junit 3/4 tests.
                     .requires("org.junit.platform.console") // Launch the JUnit Platform.
@@ -23,5 +28,47 @@ class Build {
           // if ("junit".equals(tool))
           //  arguments.put("--include-classname", "se.jbee.inject.bind.TestDiskScopeBinds");
         });
+
+    generateMavenPomXml(version);
+  }
+
+  private static void generateMavenPomXml(String version) throws Exception {
+    Files.write(
+        Path.of(".bach/workspace/se.jbee.inject@" + version + ".pom.xml"),
+        List.of(
+            "<?xml version='1.0' encoding='UTF-8'?>",
+            "<project xmlns='http://maven.apache.org/POM/4.0.0'",
+            "    xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'",
+            "    xsi:schemaLocation='http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd'>",
+            "    <modelVersion>4.0.0</modelVersion>",
+            "",
+            "    <groupId>se.jbee</groupId>",
+            "    <artifactId>se.jbee.inject</artifactId>",
+            "    <version>" + version + "</version>",
+            "",
+            "    <name>Silk DI</name>",
+            "    <description>Silk Java dependency injection framework</description>",
+            "",
+            "    <url>http://www.silkdi.com</url>",
+            "   <licenses>",
+            "        <license>",
+            "            <name>Apache License, Version 2.0</name>",
+            "            <url>https://www.apache.org/licenses/LICENSE-2.0.txt</url>",
+            "            <distribution>repo</distribution>",
+            "        </license>",
+            "    </licenses>",
+            "    <scm>",
+            "        <url>https://github.com/jbee/silk</url>",
+            "        <connection>https://github.com/jbee/silk.git</connection>",
+            "   </scm>",
+            "",
+            "    <developers>",
+            "        <developer>",
+            "            <id>jan</id>",
+            "            <name>Jan Bernitt</name>",
+            "            <email>jan@jbee.se</email>",
+            "        </developer>",
+            "    </developers>",
+            "</project>"));
   }
 }
