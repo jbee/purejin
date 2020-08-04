9d33785789d6c46e46ca8fe8dd073cfee9e63d56
Jan Bernitt <jaanbernitt@gmail.com>
2020-08-04 19:38:18 +0200

    modules for 9+

--- .bach/src/build/build/Build.java
+++ .bach/src/build/build/Build.java
@@ -21,19 +21,37 @@ class Build {
             .name("silk")
             .version(version)
             // <main>
+            .withMainSpaceUnit("src/se.jbee.inject/main/java-9", 8)
+            .withMainSpaceUnit("src/se.jbee.inject.action/main/java-9", 8)
             .withMainSpaceUnit("src/se.jbee.inject.api/main/java-9", 8)
+            .withMainSpaceUnit("src/se.jbee.inject.bind/main/java-9", 8)
+            .withMainSpaceUnit("src/se.jbee.inject.bootstrap/main/java-9", 8)
+            .withMainSpaceUnit("src/se.jbee.inject.container/main/java-9", 8)
+            .withMainSpaceUnit("src/se.jbee.inject.convert/main/java-9", 8)
+            .withMainSpaceUnit("src/se.jbee.inject.event/main/java-9", 8)
+
             .without(MainSpace.Modifier.CUSTOM_RUNTIME_IMAGE)
             .withMainSpaceJavacTweak(
-                javac -> javac.without("-Xlint").with("-Xlint:-serial,-rawtypes,-varargs"))
+                    javac -> javac.without("-Xlint").with("-Xlint:-serial,-rawtypes,-varargs")
+              .without("--class-path").with("--class-path",
+                                    ".bach/workspace/classes/11/se.jbee.inject.action" +
+                                    ":.bach/workspace/classes/11/se.jbee.inject.api" +
+                                    ":.bach/workspace/classes/11/se.jbee.inject.bind" +
+                                    ":.bach/workspace/classes/11/se.jbee.inject.bootstrap" +
+                                    ":.bach/workspace/classes/11/se.jbee.inject.container" +
+                                    ":.bach/workspace/classes/11/se.jbee.inject.convert" +
+                                    ":.bach/workspace/classes/11/se.jbee.inject.event")
+            )
+
             .withMainSpaceJavadocTweak(
-                javadoc -> javadoc.without("-Xdoclint").with("-Xdoclint:all,-missing"))
+                    javadoc -> javadoc.without("-Xdoclint").with("-Xdoclint:all,-missing"))
             // test
             .withTestSpaceUnit("src/test.integration/test/java") // extra-module tests
             .withTestSpaceUnit("src/com.example.app/test/java") // silk's first client
             .withTestSpaceUnit("src/com.example.test/test/java") // modular integration tests
             // lib/
             .withLibraryRequires(
-                "org.hamcrest", "org.junit.vintage.engine", "org.junit.platform.console");
+                    "org.hamcrest", "org.junit.vintage.engine", "org.junit.platform.console");
 
     var configuration = Configuration.ofSystem().with(Level.INFO).with(Flag.SUMMARY_LINES_UNCUT);
     new Bach(configuration, silk).build();
