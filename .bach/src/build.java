43df236c8f630c6648db701e29cabe2ef0f06bdf
Christian Stein <sormuras@gmail.com>
2021-07-11 09:30:35 +0200

    Generate API documentation

--- .bach/src/build.java
+++ .bach/src/build.java
@@ -1,7 +1,12 @@
 import com.github.sormuras.bach.Bach;
+import com.github.sormuras.bach.Call;
+import com.github.sormuras.bach.Project;
+import com.github.sormuras.bach.Settings;
 import com.github.sormuras.bach.external.JUnit;
 import com.github.sormuras.bach.project.DeclaredModule;
 import com.github.sormuras.bach.project.ProjectSpace;
+import com.github.sormuras.bach.workflow.DeclaredModuleFinder;
+import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
@@ -9,32 +14,13 @@ import java.util.Optional;
 class build {
   public static void main(String... args) {
     var cli = Cli.of(args);
-    Bach.build(
-        project ->
-            project
-                .withName("purejin")
-                .withVersion(cli.__project_version.orElse("8.2-ea"))
-                .withMainSpace(build::main)
-                .withTestSpace(build::test)
-                .withRequiresExternalModules("org.junit.platform.console")
-                .withExternalModuleLocators(JUnit.V_5_8_0_M1));
-    /*
-    -notimestamp
-    -encoding
-      UTF-8
-    -use
-    -linksource
-    -Xdoclint:-missing
-    -group
-      API
-      se.jbee.inject:se.jbee.inject.api:se.jbee.inject.bind:se.jbee.inject.lang
-    -group
-      Container
-      se.jbee.inject.bootstrap:se.jbee.inject.container
-    -group
-      Add-ons
-      se.jbee.inject.action:se.jbee.inject.event:se.jbee.inject.convert:se.jbee.inject.contract
-     */
+    var project =
+        Project.of("purejin", cli.__project_version.orElse("8.2-ea"))
+            .withMainSpace(build::main)
+            .withTestSpace(build::test)
+            .withExternalModuleLocators(JUnit.V_5_8_0_M1);
+
+    Bach.build(new PureBach(project, Settings.of()));
   }
 
   static ProjectSpace main(ProjectSpace main) {
@@ -75,14 +61,50 @@ class build {
       var unhandled = new LinkedList<String>();
       while (!todo.isEmpty()) {
         switch (todo.peek()) {
-        case "--project-version" -> {
-          todo.pop();
-          projectVersion = todo.pop();
-        }
-        default -> unhandled.add(todo.pop());
+          case "--project-version" -> {
+            todo.pop();
+            projectVersion = todo.pop();
+          }
+          default -> unhandled.add(todo.pop());
         }
       }
       return new Cli(Optional.ofNullable(projectVersion), List.copyOf(unhandled));
+    }
+  }
+
+  static class PureBach extends Bach {
+
+    public PureBach(Project project, Settings settings) {
+      super(project, settings);
+    }
+
+    @Override
+    public void executeTests() {
+      super.executeTests();
+      var main = project.spaces().main();
+      var finder = DeclaredModuleFinder.of(main.modules());
+      execute(
+          Call.tool("javadoc")
+              .with("--module", String.join(",", finder.names().toList()))
+              .with("-d", folders.workspace("documentation", "api"))
+              .with(
+                  "--module-source-path",
+                  "./*/main/java-module" + File.pathSeparator + "./*/main/java")
+              .with("--module-path", ".bach/external-modules")
+              .with("-notimestamp")
+              .with("-encoding", project.defaults().encoding())
+              .with("-use")
+              .with("-linksource")
+              .with("-Xdoclint:-missing")
+              .with(
+                  "-group",
+                  "API",
+                  "se.jbee.inject:se.jbee.inject.api:se.jbee.inject.bind:se.jbee.inject.lang")
+              .with("-group", "Container", "se.jbee.inject.bootstrap:se.jbee.inject.container")
+              .with(
+                  "-group",
+                  "Add-ons",
+                  "se.jbee.inject.action:se.jbee.inject.event:se.jbee.inject.convert:se.jbee.inject.contract"));
     }
   }
 }
