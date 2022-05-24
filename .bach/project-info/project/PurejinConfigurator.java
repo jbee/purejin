0fb11e422827f4b941fc74dfcb5835c51e7537dd
Christian Stein <sormuras@gmail.com>
2022-05-24 09:12:23 +0200

    Update project configuration
    - bump `bach` to version `8b3d3d4`
    - override `banner` with noop tool
    - use custom `junit` test execution listener

--- .bach/project-info/project/PurejinConfigurator.java
+++ .bach/project-info/project/PurejinConfigurator.java
@@ -2,22 +2,34 @@ package project;
 
 import com.github.sormuras.bach.Configurator;
 import com.github.sormuras.bach.Project;
+import com.github.sormuras.bach.ToolCallTweak;
 
 public class PurejinConfigurator implements Configurator {
 	@Override
 	public Project configureProject(Project project) {
 		return project
 				.withName("purejin")
-				.withVersion("8.2-ea")
+				.withVersion("11-ea")
 				.withTargetsJava(11)
-				.withAdditionalCompileJavacArguments("main", "-g", "-encoding",
-						"UTF-8",
-						"-parameters",
-						"-Xlint")
-				.withAdditionalCompileJavacArguments("test", "-g", "-encoding",
-						"UTF-8",
-						"-parameters",
-						"-Xlint")
-				.withExternalModules("junit", "5.8.2");
+				.withTweak(
+						ToolCallTweak.WORKFLOW_COMPILE_CLASSES_JAVAC,
+						javac -> javac
+								.with("-g")
+								.with("-encoding", "UTF-8")
+								.with("-parameters")
+								.with("-Xlint"))
+				.withTweak("test",
+						ToolCallTweak.WORKFLOW_COMPILE_CLASSES_JAVAC,
+						javac -> javac
+								.with("-g")
+								.with("-encoding", "UTF-8")
+								.with("-parameters")
+								.with("-Xlint"))
+				.withExternalModules("junit", "5.8.2")
+				.withTweak("test",
+						ToolCallTweak.WORKFLOW_TEST_JUNIT,
+						junit -> junit
+								.with("--details", "NONE")
+								.with("--disable-banner"));
 	}
 }
